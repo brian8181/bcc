7e3fa2d4797c8e4f4b617e2a80f9e7acbbc5e93b
Brian K Preston <brian8181@gmail.com>
2025-07-27 07:04:59 -0500

    add files to lines ...

---
+++ examples/lines/src/main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include "build/parser.tab.hpp"
+
+int main(int argc, char* argv[])
+{
+    // #ifdef DEBUG
+    // skip exe path ...
+   argc--; argv++;
+   if (argc > 0)
+   {
+		FILE *file;
+		file = fopen(argv[0], "r");
+		if (!file)
+        {
+			fprintf(stderr,"could not open %s\n", argv[0]);
+			exit(1);
+		}
+		yyin = file;
+	}
+    else
+    {
+        fprintf(stderr, "\nMissing filename paramter, help ->\n");
+        fprintf(stderr, "lex [OPTION]... [FilE]...\n");
+        fprintf(stderr, "Interactive mode...\n\n");
+        yyin = stdin;
+    }
+
+    yyparse();
+    // #endif
+
+    return 0;
+}
